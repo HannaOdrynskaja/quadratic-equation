31b3306b4e137a15d9e6e238ed4de1e93f0e2625
Hanna Odrynskaja <36698550+HannaOdrynskaja@users.noreply.github.com>
2018-02-28 14:21:14 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+var roof = [];
+var str = ""+equation;
+var str = str.replace(/ /g, '');
+var str = str.replace('*x^2+', ',');
+var str = str.replace('*x^2', ',');
+var str = str.replace('*x+', ',');
+var str = str.replace('*x', ',');
+var arr = str.split(',');
+var a = Number(arr[0]);
+var b = Number(arr[1]);
+var c = Number(arr[2]);
+var d = b*b-4*a*c;
+if (d<0)
+	{console.log ('there are not roofs');
+	}
+if (d==0)
+	{
+		roof[0] = (-b)/(2*a);
+	}		
+if (d>0){
+		
+		var r0 = Math.round(((-b)-Math.sqrt(d))/(2*a));
+		var r1 = Math.round(((-b)+Math.sqrt(d))/(2*a));
+		if((r0-r1)<0)
+		{
+		roof[0]= r0;
+		roof[1]= r1;
+	
+		}else{
+		roof[0]= r1;
+		roof[1]= r0;
+		}
+	}
+	  	  return roof;
+}		  
+
